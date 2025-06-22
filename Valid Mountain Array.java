6785a8861510b79bdf5b5dbb063dd3be4df57534
Shrivali Dutt <149003676+shrivalidutt@users.noreply.github.com>
2025-06-22 14:53:31 +0530

    Create Valid Mountain Array.java

---
+++ Valid Mountain Array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+        if(arr.length < 3) return false;
+
+        int left = 0;
+        int right = arr.length-1;
+
+        while(left < arr.length-1 && arr[left] < arr[left + 1]){
+            left++;
+        }
+        while(right >= 1 && arr[right] < arr[right - 1]){
+            right--;
+        }
+        return left == right && left>0 && right<arr.length-1;
+    }
+}
